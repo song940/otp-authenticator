5b04a88d9b61348179cf068f8befef9fc8feb877
Lsong <song940@gmail.com>
2023-11-08 23:55:37 +0800

    update

--- app.js
+++ app.js
@@ -1,55 +1,21 @@
-import { ready } from 'https://lsong.org/scripts/dom.js';
-import { h, render, useState, useEffect } from 'https://lsong.org/scripts/react/index.js';
-
 import "https://lsong.org/js/application.js";
-
-export function base32ToBuffer(base32) {
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
-  let bits = '';
-  let result = new Uint8Array(Math.ceil(base32.length * 5 / 8));
-
-  for (let i = 0; i < base32.length; i++) {
-    let value = alphabet.indexOf(base32[i].toUpperCase());
-    bits += value.toString(2).padStart(5, '0');
-  }
-
-  for (let i = 0; i < result.length; i++) {
-    result[i] = parseInt(bits.substr(i * 8, 8), 2);
-  }
-  return result;
-}
-
-export async function computeHMAC(key, data) {
-  const cryptoKey = await crypto.subtle.importKey('raw', key, {
-    name: 'HMAC',
-    hash: 'SHA-1',
-  }, false, ['sign']);
-  return crypto.subtle.sign('HMAC', cryptoKey, data);
-}
+import { ready } from 'https://lsong.org/scripts/dom.js';
+import { sha1hmac } from 'https://lsong.org/scripts/crypto/sha.js';
+import { base32decode } from 'https://lsong.org/scripts/crypto/base32.js';
+import { h, render, useState, useEffect, useLocalStorageState } from 'https://lsong.org/scripts/react/index.js';
 
 export async function generateTOTP(secret) {
   const epoch = Math.floor(Date.now() / 1000);
   const time = new Uint8Array(new DataView(new ArrayBuffer(8)).buffer);
   new DataView(time.buffer).setUint32(4, Math.floor(epoch / 30));
-  const key = base32ToBuffer(secret);
-  const hmac = await computeHMAC(key, time.buffer);
+  const key = base32decode(secret);
+  const hmac = await sha1hmac(key, time.buffer);
   const offset = new Uint8Array(hmac)[19] & 0xf;
   const otp = new DataView(hmac).getUint32(offset) & 0x7fffffff;
   const code = (otp % 1000000).toString().padStart(6, '0');
   return code;
 }
 
-function useLocalStorageState(key, defaultValue) {
-  const [state, setState] = useState(defaultValue);
-  const storedValue = localStorage.getItem(key);
-  useEffect(() => {
-    setState(storedValue !== null ? JSON.parse(storedValue) : defaultValue);
-  }, []);
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
-  return [state, setState];
-}
 const SiteItem = ({ site }) => {
   const [otp, setOtp] = useState('');
   // Function to update the OTP for this site
